44c2c27512373a14d295d41c91ebf201d8866711
Tushar Desai <tushardesai@Tushars-MacBook-Pro.local>
2020-04-03 15:24:54 -0400

    black and white imaging

--- final/stitch_images.py
+++ final/stitch_images.py
@@ -5,19 +5,16 @@ from matplotlib import pyplot as plt
 
 
 directory = os.fsencode('data')
-#255 represents a white cell
-#0 represents a black cell
-composite_image_list = [255 for i in range(784)]
+#255 represents a black cell
+#0 represents a white cell
+composite_image_list = [0 for i in range(784)]
 
 for file in os.listdir(directory):
   filename = os.fsdecode(file)
   pixel_extractor = PixelExtractor("data/" + filename, 'g')
   target_pixel_locations = pixel_extractor.extract_target_pixel_location()
   for loc in target_pixel_locations:
-    composite_image_list[loc] = 0
-
-
-
+    composite_image_list[loc] = 255
 
 
 reshaped_composite_image = []
@@ -26,7 +23,7 @@ while n <= len(composite_image_list):
     reshaped_composite_image.append(composite_image_list[n-28:n])
     n+=28
 
-plt.imshow(reshaped_composite_image, interpolation='nearest')
+plt.imshow(reshaped_composite_image, cmap='Greys',  interpolation='nearest')
 plt.savefig('MNIST_IMAGE.png')#save MNIST image
 plt.show()#Show / plot that image
 
